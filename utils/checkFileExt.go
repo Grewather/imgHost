3f7ca5312140373a6fe53708cbc7ea211e646e03
Grewather <69985703+Grewather@users.noreply.github.com>
2024-02-21 18:16:58 +0100

    Added dots (i swear it worked before)

--- utils/checkFileExt.go
+++ utils/checkFileExt.go
@@ -1,8 +1,11 @@
 package utils
 
+import "fmt"
+
 func CheckFileExt(extension string) bool {
+	fmt.Println(extension)
 	switch extension {
-	case "png", "jpg", "jpeg", "webp", "gif":
+	case ".png", ".jpg", ".jpeg", ".webp", ".gif":
 		return true
 	}
 	return false
